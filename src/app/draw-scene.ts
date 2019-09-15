d04499f4eb0aa78ceefb9c23cb1412ce18526007
Unknown <justynasadlak10@gmail.com>
2019-09-15 12:06:16 +0200

    Improve drawing

--- src/app/draw-scene.ts
+++ src/app/draw-scene.ts
@@ -6,23 +6,23 @@ export function drawScene(surface: Surface) {
   const points = {
     screen: [0, 0, 1080, 1794],
     elements: [
-      [0, 0, 1080, 1794, "android.widget.FrameLayout"],
-      [0, 0, 1080, 1794, "android.widget.LinearLayout"],
-      [0, 63, 1080, 1794, "android.widget.FrameLayout"],
-      [0, 63, 1080, 1794, "android.view.ViewGroup"],
-      [0, 63, 1080, 210, "android.widget.FrameLayout"],
-      [0, 63, 1080, 210, "android.view.ViewGroup"],
-      [42, 101, 519, 172, "android.widget.TextView"],
-      [0, 210, 1080, 1794, "android.widget.FrameLayout"],
-      [0, 210, 1080, 1794, "android.view.ViewGroup"],
-      [0, 210, 1080, 1794, "android.widget.LinearLayout"],
-      [0, 210, 131, 341, "android.widget.Button"],
-      [0, 341, 394, 472, "android.widget.Button"],
-      [0, 472, 919, 525, "android.widget.Button"],
-      [0, 525, 26, 551, "android.widget.Button"],
-      [0, 551, 121, 661, "android.widget.Button"],
-      [0, 0, 1080, 63, "android.view.View"],
-      [0, 0, 0, 0, "android.view.View"]]
+      [0, 0, 1080, 1794],
+      [0, 0, 1080, 1794],
+      [0, 63, 1080, 1794],
+      [0, 63, 1080, 1794],
+      [0, 63, 1080, 210],
+      [0, 63, 1080, 210],
+      [42, 101, 519, 172],
+      [0, 210, 1080, 1794],
+      [0, 210, 1080, 1794],
+      [0, 210, 1080, 1794],
+      [0, 210, 131, 341],
+      [0, 341, 394, 472],
+      [0, 472, 919, 525],
+      [0, 525, 26, 551],
+      [0, 551, 121, 661],
+      [0, 0, 1080, 63],
+      [0, 0, 0, 0]]
   }
 
   // Create the square border by drawing a straight path
@@ -52,12 +52,22 @@ export function drawScene(surface: Surface) {
   group.append(pathScreen, image);
 
   points.elements.forEach(element => {
-    let path = new Path({
+    let path;
+    if(element[3] < 64){
+    path = new Path({
       stroke: {
         color: "red",
+        width: 5
+      }
+    });
+  } else {
+    path = new Path({
+      stroke: {
+        color: "blue",
         width: 2
       }
     });
+  }
 
     path.moveTo(element[0], element[1])
       .lineTo(element[2], element[1]).lineTo(element[2], element[3]).lineTo(element[0], element[3])
